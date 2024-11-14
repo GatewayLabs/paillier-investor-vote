91c10b9e48c3e967abd2baf8acdebda1e4748f56
Nuno Carvalho <nuno@mygateway.xyz>
2024-11-14 19:11:07 +0000

    feat: add abi for contract, contract address and chain ID as part of the helpers

--- app/helpers.ts
+++ app/helpers.ts
@@ -23,15 +23,735 @@ export const pizzaToppings = [
   { id: 19, name: "Roasted red peppers" },
   { id: 20, name: "Feta cheese" },
   { id: 21, name: "Ricotta" },
-  { id: 22, name: "Hot honey" },
 ];
 
 export const contractABI = [
-  "function vote(bytes[] calldata encryptedVoteValues, uint256[] calldata toppingIds) external",
-  "function getVotes() public view returns (uint256[])",
+  {
+    inputs: [
+      {
+        internalType: "bytes",
+        name: "n",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes",
+        name: "g",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "voter",
+        type: "address",
+      },
+    ],
+    name: "VoteCast",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "b",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "add",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        internalType: "uint256",
+        name: "b",
+        type: "uint256",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "add_const",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "newTotalToppings",
+        type: "uint256",
+      },
+    ],
+    name: "changeTotalToppings",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "encValue",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "lambda",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "mu",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PrivateKey",
+        name: "privateKey",
+        type: "tuple",
+      },
+      {
+        internalType: "bytes",
+        name: "sigma",
+        type: "bytes",
+      },
+    ],
+    name: "decrypt",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        internalType: "uint256",
+        name: "b",
+        type: "uint256",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "div_const",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "rnd",
+        type: "bytes",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "encrypt",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes",
+        name: "rnd",
+        type: "bytes",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "encryptZero",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "encryptedVoteSums",
+    outputs: [
+      {
+        internalType: "bytes",
+        name: "value",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "hasVoted",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        internalType: "uint256",
+        name: "b",
+        type: "uint256",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "mul_const",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "publicKey",
+    outputs: [
+      {
+        internalType: "bytes",
+        name: "n",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes",
+        name: "g",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "b",
+        type: "tuple",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "sub",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "value",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct Ciphertext",
+        name: "a",
+        type: "tuple",
+      },
+      {
+        internalType: "int256",
+        name: "b",
+        type: "int256",
+      },
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "n",
+            type: "bytes",
+          },
+          {
+            internalType: "bytes",
+            name: "g",
+            type: "bytes",
+          },
+        ],
+        internalType: "struct PublicKey",
+        name: "publicKey",
+        type: "tuple",
+      },
+    ],
+    name: "sub_const",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bytes",
+            name: "val",
+            type: "bytes",
+          },
+          {
+            internalType: "bool",
+            name: "neg",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "bitlen",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct BigNumber",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalToppings",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes[]",
+        name: "encryptedVoteVector",
+        type: "bytes[]",
+      },
+    ],
+    name: "vote",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
 const publicKeyN = BigInt("0x" + process.env.NEXT_PUBLIC_PUBLIC_KEY_N);
 const publicKeyG = BigInt("0x" + process.env.NEXT_PUBLIC_PUBLIC_KEY_G);
 
 export const publicKey = new PublicKey(publicKeyN, publicKeyG);
+export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!;
+export const SHIELD_TESTNET_CHAIN_ID = "0xa5b5a";
